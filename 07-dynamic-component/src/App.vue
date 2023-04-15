af47ebb2341bbfba07a548f51d0a15981e1668bf
derson26 <ussuale26@gmail.com>
2023-04-15 22:17:35 +0100

    Dynamic Component in Vue

---
+++ 07-dynamic-component/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <ul>
+      <li v-on:click="getItem">Home</li>
+      <li v-on:click="getItem">About</li>
+      <li v-on:click="getItem">Contact</li>
+    </ul>
+  </div>
+
+  <component :is="currentView"/>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue';
+  import About from './components/About.vue';
+  import Contact from './components/Contact.vue';
+  import Home from './components/Home.vue'; 
+
+  export default defineComponent({
+    components: { Home, Contact, About},
+    data() {
+      return {
+        currentView: 'Home'
+      }
+    },
+    methods:{
+      getItem(event:Event) {
+        const li = event.target as HTMLLIElement;
+        // console.log(li.innerText)
+        this.currentView = li.innerText;
+      }
+    }
+  })
+</script>
+
+<style scoped>
+  li {
+    cursor: pointer;
+    display: inline-block;
+    padding: 0 .3em;
+  }
+
+  ul {
+    list-style: none;
+  }
+</style>
---
+++ 07-dynamic-component/src/components/Home.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>Home</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({})
+</script>
+
+<style scoped>
+
+</style>
