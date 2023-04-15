d1ebf756329a37a2c61314f08bc795e674d8f38a
derson26 <ussuale26@gmail.com>
2023-04-15 13:27:38 +0100

    Create Custom Component with props

---
+++ 04-create-components/src/App.vue
@@ -0,0 +1,23 @@
+
+import ButtonStyled from './components/ButtonStyled.vue';
+
+<template>
+  <ButtonStyled color="success"></ButtonStyled>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import ButtonStyled from "./components/ButtonStyled.vue";
+
+export default defineComponent({
+  components: {
+    ButtonStyled,
+  },
+});
+</script>
+<style scoped>
+/* Child Styled */
+  .button-styled ::v-deep(span)  {
+    color: black;
+  }
+</style>
---
+++ 04-create-components/src/components/ButtonStyled.vue
@@ -0,0 +1,52 @@
+<template>
+  <button :class="['button-styled', color]">
+    Button
+    <span>Derson Ussuale</span>
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+type TColor = 'primary' | 'danger' | 'success';
+
+export default defineComponent({
+  props: {
+    color: {
+      type: String, // or [String, Number, Object]
+      default: 'primary',
+      // required: false
+      validator: (value: TColor) => {
+        return ['primary', 'danger', 'success'].includes(value)
+      }
+    },
+    
+  }
+})
+</script>
+
+<style scoped>
+  .button-styled {
+  color: #fff;
+  border-radius: 0.3rem;
+  padding: 0.5rem 1rem;
+  outline: none;
+  border: none;
+  cursor: pointer;
+  display: block;
+  margin-bottom: 1rem;
+  /* background-color: #2d6cea; */
+}
+
+.success {
+  background-color: green;
+}
+
+.danger {
+  background-color: red;
+}
+
+.primary {
+  background-color: #2d6cea;
+}
+</style>
