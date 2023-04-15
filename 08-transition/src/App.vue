450a777f5ccaf60522e81f34bb26ba206dd10d1b
derson26 <ussuale26@gmail.com>
2023-04-15 23:57:42 +0100

    transition in vue

---
+++ 08-transition/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <button @click="fShow">Toggle</button>
+
+  <transition>
+    <p v-show="show">I am Derson Ussuale, and i Love VueJs a lot</p>
+  </transition>
+  
+</template>
+ 
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  data() {
+    return {
+      show: true,
+    }
+  },
+  methods: {
+    fShow() {
+      this.show = !this.show;
+    }
+  }
+})
+</script>
+
+<style scoped>
+  .v-enter-from, .v-leave-to { opacity:0}
+  .v-enter-active, .v-leave-active { transition: opacity 0.5s;}
+  /* .v-leave-active { transition: opacity 0.5s} */
+  /* .v-leave-to { opacity:0 } */
+
+</style>
