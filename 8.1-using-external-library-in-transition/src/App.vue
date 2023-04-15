5d24984bb8936a0fdce0283b770dcb8452c4654f
derson26 <ussuale26@gmail.com>
2023-04-16 00:17:57 +0100

    using external library in transition

---
+++ 8.1-using-external-library-in-transition/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <button @click="fShow">Toggle</button>
+
+  <transition
+    enter-active-class="animate__animated animate__fadeInLeft"
+    leave-active-class="animate__animated animate__fadeInRight">
+    <p v-show="show">I am Derson Ussuale, and i Love VueJs a lot</p>
+  </transition>
+  
+</template>
+ 
+<script lang="ts">
+import { defineComponent } from 'vue'
+import 'animate.css';
+
+export default defineComponent({
+  data() {
+    return {
+      show: true,
+    }
+  },
+  methods: {
+    fShow() {
+      this.show = !this.show;
+    }
+  }
+})
+</script>
+
+<style scoped></style>
