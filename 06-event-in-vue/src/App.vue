a551fc8441cebd9d45086e4279dba424a0aa7abc
derson26 <ussuale26@gmail.com>
2023-04-15 19:00:32 +0100

    Passing data with Event

---
+++ 06-event-in-vue/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <Button @passingUsers="action($event)"></Button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Button from './components/Button.vue';
+
+type IUser = {
+  name: string,
+  id: number,
+}
+
+export default defineComponent({
+  components: {
+    Button,
+  },
+
+  methods: {
+    action(event: IUser) {
+      console.log(event)
+    }
+  }
+})
+</script>
+
+<style scoped></style>
---
+++ 06-event-in-vue/src/components/Button.vue
@@ -0,0 +1,20 @@
+<template>
+  <button v-on:click.once="$emit('passingUsers', users)">button</button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  data() {
+    return {
+      users: {
+        name: "Derson Ussuale",
+        id:1,
+      }
+    }
+  }
+})
+</script>
+
+<style scoped></style>
